3b4af0eaf3ed7b673d385a1c4d60e76017400fbe
abusizhishen <abusizhishen@gmail.com>
2021-02-27 00:00:06 +0800

    readme

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -13,21 +13,20 @@ func main() {
 
 type Rule struct {
 	rule string
-	p *parser.RuleParser
-	v *src.RuleEngineVisitor
+	p    *parser.RuleParser
+	v    *src.RuleEngineVisitor
 }
 
 var ruleMap = make(map[string]string)
 
-func http()  {
+func http() {
 	var g = gin.Default()
 	g.POST("/create", createRule)
 	g.POST("/calculate", calculateRule)
 	g.GET("/show", showRule)
 
-
 	err := g.Run(":80")
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 }
@@ -38,45 +37,45 @@ type Create struct {
 }
 
 type Calculate struct {
-	Name string `json:"name" binding:"required"`
+	Name string                 `json:"name" binding:"required"`
 	Data map[string]interface{} `json:"data" binding:"required"`
 }
 
-func createRule(ctx *gin.Context)  {
+func createRule(ctx *gin.Context) {
 	var create Create
 	err := ctx.ShouldBindJSON(&create)
-	if err != nil{
-		ctx.String(200,err.Error())
+	if err != nil {
+		ctx.String(200, err.Error())
 		return
 	}
 
 	ruleMap[create.Name] = create.Rule
-	ctx.JSON(200,nil)
+	ctx.JSON(200, nil)
 }
 
-func calculateRule(ctx *gin.Context)  {
+func calculateRule(ctx *gin.Context) {
 	var calculate Calculate
 	err := ctx.ShouldBindJSON(&calculate)
-	if err != nil{
-		ctx.String(200,err.Error())
+	if err != nil {
+		ctx.String(200, err.Error())
 		return
 	}
 
-	r,ok := ruleMap[calculate.Name]
-	if !ok{
-		ctx.String(200,"无效的规则: "+ calculate.Name)
+	r, ok := ruleMap[calculate.Name]
+	if !ok {
+		ctx.String(200, "无效的规则: "+calculate.Name)
 		return
 	}
 	v := src.NewVisitor(calculate.Data)
 	p := getParser(r)
-	ctx.JSON(200,map[string]interface{}{"data":p.Init().Accept(v)})
+	ctx.JSON(200, map[string]interface{}{"data": p.Init().Accept(v)})
 }
 
-func showRule(ctx *gin.Context)  {
-	ctx.JSON(200,ruleMap)
+func showRule(ctx *gin.Context) {
+	ctx.JSON(200, ruleMap)
 }
 
-func getParser(rule string) *parser.RuleParser{
+func getParser(rule string) *parser.RuleParser {
 	input := antlr.NewInputStream(rule)
 	lex := parser.NewRuleLexer(input)
 	tokens := antlr.NewCommonTokenStream(lex, antlr.TokenDefaultChannel)
@@ -86,9 +85,9 @@ func getParser(rule string) *parser.RuleParser{
 	return p
 }
 
-func resp(ctx *gin.Context ,data interface{})  {
-	if data == nil{
+func resp(ctx *gin.Context, data interface{}) {
+	if data == nil {
 		data = map[string]string{}
 	}
-	ctx.JSON(200,data)
+	ctx.JSON(200, data)
 }
---
+++ readme.md
@@ -0,0 +1,27 @@
+#ruleEngine
+
+## 规则引擎
+
+### 运算
+- [x] 算术运算 加减乘除
+  - [x] float64 op float64
+- [x] 比较操作 ``` >、>=、==、<=、<```
+- [x] 逻辑运算 ```&&、||、! ```
+
+### 语句
+- [x] if、elsif、else
+- [x] return
+
+### 数据类型
+- [x] bool
+- [x] string
+- [ ] number
+  - [x] float64
+- [ ] struct
+- [ ] map
+- [ ] list
+
+##开发工具
+- goland
+- go
+- antlr
