f4aecafa615dcf704a70d8ebdbdc324953b71a5c
abusizhishen <abusizhishen@gmail.com>
2021-03-05 23:35:26 +0800

    支持字符串

--- src/string.go
+++ src/string.go
@@ -1,1 +1,17 @@
 package src
+
+import (
+	"fmt"
+	"github.com/abusizhishen/ruleEngine/parser"
+)
+
+func (v *RuleEngineVisitor) VisitSTRING(ctx *parser.STRINGContext) interface{} {
+	fmt.Println("VisitSTRING", ctx.GetText())
+	v.push(ctx.Str().GetText())
+	return nil
+}
+
+func (v *RuleEngineVisitor) VisitStringValue(ctx *parser.StringValueContext) interface{} {
+
+	return nil
+}
